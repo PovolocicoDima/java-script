e2c245574032896a3cf65a50816d547ba436b3cb
PovolocicoDima <povolocico.dima@gmail.com>
2020-05-25 20:00:07 +0300

    regexp_finish

--- regexp/test.js
+++ regexp/test.js
@@ -1,12 +1,5 @@
-let stock = "1 lemon, 2 cabbages, and 101 eggs";
-function minusOne(match, amount, unit) {
-    amount = Number(amount) - 1;
-    if (amount == 1) {
-        unit = unit.slice(0, unit.length - 1);
-    } else if (amount == 0) {
-        amount = "no";
-    }
-    return amount + " " + unit ;
-}
+let input = "Sring with 3 digits .... 42 and 88.";
+let number = /\b\d+\b/g;
+let match;
 
-console.log(stock.replace(/(\d+) (\w+)/g, minusOne));
+while ()
